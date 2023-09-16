6c472a80b739af8aec81ad76a41519fada5b89cc
배만진 <gentlemmj@gmail.com>
2023-09-16 21:54:51 +0900

    test: '회원별 적립금 적립' 기능에 대한 통합 테스트 수정

--- src/test/kotlin/com/marketboro/controller/EarnPointIntegrationTest.kt
+++ src/test/kotlin/com/marketboro/controller/EarnPointIntegrationTest.kt
@@ -7,6 +7,7 @@ import com.marketboro.domain.PointAccount
 import com.marketboro.infra.PointAccountJpaRepository
 import io.kotest.core.spec.style.FunSpec
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,6 +18,7 @@ class EarnPointIntegrationTest(
     val existingMemberId = MemberId(EXISTING_MEMBER_ID)
     val notExistingMemberId = MemberId(NOT_EXISTING_MEMBER_ID)
     lateinit var pointAccount: PointAccount
+    lateinit var req: TestPointTransactionReq
 
     beforeTest {
         accountRepository.deleteAll()
@@ -27,17 +29,28 @@ class EarnPointIntegrationTest(
 
 
     test("등록된 회원은 적립금을 적립할 수 있다.") {
+        req = TestPointTransactionReq(points = 10)
+
         testClient.post()
             .uri("/api/members/$existingMemberId/points")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(req)
             .exchange()
             .expectStatus().is2xxSuccessful
     }
 
     test("등록되지 않은 회원은 적립금을 적립할 수 없다.") {
+        req = TestPointTransactionReq(points = 10)
+
         testClient.post()
             .uri("/api/members/$notExistingMemberId/points")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(req)
             .exchange()
             .expectStatus().is4xxClientError
     }
-
 })
+
+data class TestPointTransactionReq(
+    val points: Long
+)
