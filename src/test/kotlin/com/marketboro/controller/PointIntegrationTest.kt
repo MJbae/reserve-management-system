ce8afac09fa3dc264b9bd454123bfa2e63d5d828
배만진 <gentlemmj@gmail.com>
2023-09-16 15:28:18 +0900

    test: '회원별 적립금 합계 조회' 기능에 대한 통합 테스트 정의

--- src/test/kotlin/com/marketboro/controller/PointIntegrationTest.kt
+++ src/test/kotlin/com/marketboro/controller/PointIntegrationTest.kt
@@ -1,5 +1,8 @@
 package com.marketboro.controller
 
+import com.marketboro.domain.MemberId
+import com.marketboro.domain.PointAccount
+import com.marketboro.infra.PointAccountJpaRepository
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.context.SpringBootTest
@@ -9,12 +12,20 @@ import org.springframework.test.web.reactive.server.WebTestClient
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PointIntegrationTest(
     private val testClient: WebTestClient,
+    private val accountRepository: PointAccountJpaRepository
 ) : FunSpec({
-    test("유효한 memeberId로 적립금 합계를 조회할 수 있다.") {
-        val memberId = 1L
+    val existingMemberId = MemberId("existingId")
+    val notExistingMemberId = MemberId("notExistingId")
 
+    beforeTest {
+        accountRepository.deleteAll()
+        accountRepository.save(PointAccount(existingMemberId))
+    }
+
+
+    test("등록된 회원의 적립금 합계를 조회할 수 있다.") {
         val res = testClient.get()
-            .uri("/api/members/${memberId}/points/total")
+            .uri("/api/members/$existingMemberId/points/total")
             .exchange()
             .expectStatus().is2xxSuccessful
             .expectBody(object : ParameterizedTypeReference<TestTotalPointsDto>() {})
@@ -22,6 +33,14 @@ class PointIntegrationTest(
 
         res.totalPoints shouldBe 0
     }
+
+    test("등록되지 않은 회원의 적립금 합계를 조회할 수 없다.") {
+        testClient.get()
+            .uri("/api/members/$notExistingMemberId/points/total")
+            .exchange()
+            .expectStatus().is4xxClientError
+    }
+
 })
 
 data class TestTotalPointsDto(
