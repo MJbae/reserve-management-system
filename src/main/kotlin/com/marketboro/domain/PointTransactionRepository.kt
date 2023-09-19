0b2ec99ab206b77a62513697f168659a085569bd
배만진 <gentlemmj@gmail.com>
2023-09-19 20:19:14 +0900

    refactor: '회원별 적립금 내역 조회'에 페이징 개선(Page -> Slice)

--- src/main/kotlin/com/marketboro/domain/PointTransactionRepository.kt
+++ src/main/kotlin/com/marketboro/domain/PointTransactionRepository.kt
@@ -1,9 +1,9 @@
 package com.marketboro.domain
 
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Slice
 
 interface PointTransactionRepository {
-    fun findByLatest(accountId: AccountId, types: Set<TransactionType>, pageReq: PageRequest): Page<PointTransaction>
+    fun findAll(accountId: AccountId, types: Set<TransactionType>, pageReq: PageRequest): Slice<PointTransaction>
     fun save(transaction: PointTransaction)
 }
--- src/main/kotlin/com/marketboro/usecase/dto/PointHistoryDto.kt
+++ src/main/kotlin/com/marketboro/usecase/dto/PointHistoryDto.kt
@@ -2,23 +2,21 @@ package com.marketboro.usecase.dto
 
 import com.marketboro.domain.PointTransaction
 import com.marketboro.domain.TransactionType
-import org.springframework.data.domain.Page
+import org.springframework.data.domain.Slice
 
 data class PointHistoryDto(
     val transactions: List<TransactionDto>,
-    val totalElements: Long,
-    val totalPages: Int,
-    val currentPage: Int
+    val currentPage: Int,
+    val hasNextPage: Boolean
 ) {
-    constructor(transactionsPage: Page<PointTransaction>) : this(
-        transactions = transactionsPage.content.map { TransactionDto(it.type, it.points) },
-        totalElements = transactionsPage.totalElements,
-        totalPages = transactionsPage.totalPages,
-        currentPage = transactionsPage.number
+    constructor(transSlice: Slice<PointTransaction>) : this(
+        transactions = transSlice.content.map { TransactionDto(it.type, it.points) },
+        currentPage = transSlice.number,
+        hasNextPage = transSlice.hasNext(),
     )
 }
 
 data class TransactionDto(
     val type: TransactionType,
-    val amount: Long
+    val amount: Long,
 )
