8e5dab2667f8203047d0b940291fb187a972524e
배만진 <gentlemmj@gmail.com>
2023-09-19 20:25:05 +0900

    feat: PointTransaction 조회 성능을 고려한 인덱스 설정

--- src/main/kotlin/com/marketboro/domain/PointTransaction.kt
+++ src/main/kotlin/com/marketboro/domain/PointTransaction.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.*
 import java.time.LocalDateTime
 
 @Entity
+@Table(indexes = [Index(name = "idx_type_created_at", columnList = "type, createdAt")])
 class PointTransaction(
     @Embedded
     @AttributeOverride(name = "id", column = Column(name = "account_id", length = 40))
@@ -14,11 +15,11 @@ class PointTransaction(
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    var type: TransactionType,
+    val type: TransactionType,
 
     @Column(nullable = false)
-    val createdAt: LocalDateTime = LocalDateTime.now()
-){
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+) {
     @Id
     @TableGenerator(name = "PointTransactionIdGenerator", table = "sequence", allocationSize = 100)
     @GeneratedValue(strategy = GenerationType.TABLE, generator = "PointTransactionIdGenerator")
